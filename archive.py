6e47223de5bf8ed169fe14455c26864fe4e3f911
InnovativeInventor <theinnovativeinventor@gmail.com>
2019-06-08 09:26:13 -0400

    Added option for a shallow archive

--- archive.py
+++ archive.py
@@ -1,11 +1,14 @@
-import tweepy
-import json
 import datetime
+import json
 import time
+
 import GetOldTweets3 as got
+import tweepy
+
 
 class ArchiveTweet:
-    def __init__(self, consumer_token, consumer_secret, access_token, access_token_secret):
+    def __init__(self, consumer_token, consumer_secret, access_token,
+                 access_token_secret):
         auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth, wait_on_rate_limit=True)
@@ -18,24 +21,31 @@ class ArchiveTweet:
     def archive_account(self, query: str):
         ids = []
         hashtags = []
-        for each_status in tweepy.Cursor(self.api.user_timeline, screen_name = query, count = 1000).items():
+        for each_status in tweepy.Cursor(
+                self.api.user_timeline, screen_name=query, count=1000).items():
             ids.append(each_status.id_str + '\n')
             for each_hashtag in each_status.entities.get('hashtags'):
-                if each_hashtag['text'] not in hashtags: # Can't wait for 3.8
+                if each_hashtag['text'] not in hashtags:  # Can't wait for 3.8
                     hashtags.append(each_hashtag['text'] + '\n')
                 if self.debug:
-                    print(each_status.user.screen_name, each_status.text.strip('\n'), end='\n\n')
+                    print(
+                        each_status.user.screen_name,
+                        each_status.text.strip('\n'),
+                        end='\n\n')
         return ids, hashtags
 
-    def archive_hashtag(self, query: str):
+    def archive_hashtag(self, query: str, full_backup=True):
         ids = []
-        for each_tweet in got.manager.TweetManager.getTweets(got.manager.TweetCriteria().setQuerySearch(query)):
-            ids.append(str(each_tweet.id)+'\n')
-        for each_status in tweepy.Cursor(self.api.search, q=query, count=100).items():
+        if full_backup:
+            for each_tweet in got.manager.TweetManager.getTweets(
+                    got.manager.TweetCriteria().setQuerySearch(query)):
+                ids.append(str(each_tweet.id) + '\n')
+
+        for each_status in tweepy.Cursor(
+                self.api.search, q=query, count=100).items():
             ids.append(each_status.id_str + '\n')
         return ids
 
     def write_data(self, filename, data):
         with open(filename, 'a') as outfile:
             outfile.writelines(data)
-
--- archiver.py
+++ archiver.py
@@ -1,22 +1,30 @@
 from archive import ArchiveTweet
 
 # Consumer API, Consumer secret, Access token, Access token secret
-archiver = ArchiveTweet('ram5ry9JGyleYXnm0x3wrkgZk', '2PnlOACLHGN3BDodpaV3RvaYOuCcDRCygcamDoJ6Q8SuucM5db', '944986243296882688-QwaMJx5UBB3Uj2o8Qjd2gjKzDp5GRKx', 'jZ5RquDuBLR4RcpSz2lv4Acgyk4L4IUVkSOhuzDDUX31C')
+archiver = ArchiveTweet('ram5ry9JGyleYXnm0x3wrkgZk',
+                        '2PnlOACLHGN3BDodpaV3RvaYOuCcDRCygcamDoJ6Q8SuucM5db',
+                        '944986243296882688-QwaMJx5UBB3Uj2o8Qjd2gjKzDp5GRKx',
+                        'jZ5RquDuBLR4RcpSz2lv4Acgyk4L4IUVkSOhuzDDUX31C')
 
 # Official tweets
 with open("official_accounts.txt", "r") as filename:
     for each_account in filename:
         ids, hashtags = archiver.archive_account(each_account)
-        hashtags = [x for x in hashtags if "choate" in x.lower() or "crh" in x.lower() or "wildboar" in x.lower()]
+        hashtags = [
+            x for x in hashtags if "choate" in x.lower() or "crh" in x.lower()
+            or "wildboar" in x.lower()
+        ]
         print(len(ids))
         archiver.write_data('official_tweets.txt', ids)
         archiver.write_data('related_tweets.txt', ids)
         archiver.write_data('related_terms.txt', hashtags)
 
-# Related tweets 
+# Related tweets
 with open("related_terms.txt", "r") as filename:
     for each_hashtag in filename:
-        ids = archiver.archive_hashtag(each_hashtag.rstrip())
-        ids += archiver.archive_hashtag('#' + each_hashtag.rstrip())
+        ids = archiver.archive_hashtag(
+            each_hashtag.rstrip(), full_backup=False)
+        ids += archiver.archive_hashtag(
+            '#' + each_hashtag.rstrip(), full_backup=False)
         archiver.write_data('related_tweets.txt', ids)
-        print(each_hashtag.rstrip(),'#' + each_hashtag.rstrip(), len(ids))
+        print(each_hashtag.rstrip(), '#' + each_hashtag.rstrip(), len(ids))
